b721be5e0ff671645ff8be1ca8e914fe33bc3ccf
Lord-Bambucha <lordbambycha93@gmail.com>
2022-10-01 18:34:36 +0300

    the end

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -7,6 +7,7 @@
     const isMenuOpen =
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+          document.body.classList.toggle("modal-open");
     mobileMenu.classList.toggle('is-open');
 
     const scrollLockMethod = !isMenuOpen
@@ -17,6 +18,7 @@
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
+
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
